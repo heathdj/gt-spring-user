229ca54caa79085228dbf0037b786621d5822dce
David Heath <heathdj@gmail.com>
2019-08-23 21:36:41 -0700

    Initial Service Files and beginning of application settings.

---
+++ src/main/java/net/bearcreekconsulting/geektrek/user/gtspringuser/GtSpringUserApplication.java
@@ -0,0 +1,13 @@
+package net.bearcreekconsulting.geektrek.user.gtspringuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtSpringUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtSpringUserApplication.class, args);
+    }
+
+}
