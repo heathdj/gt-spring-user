9986fbb979b7a0e4104c847d3b9309e969d5f5c0
David Heath <heathdj@gmail.com>
2019-08-24 22:27:56 -0700

    Setup Junit5. closes #4

---
+++ src/test/java/net/bearcreekconsulting/geektrek/user/gtspringuser/web/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package net.bearcreekconsulting.geektrek.user.gtspringuser.web.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+/**
+ * Created by David on 8/24/2019. Copyright (c) 2019 Bear Creek Consulting LLC.
+ */
+@Disabled
+
+
+
+public class UserControllerTest {
+}
